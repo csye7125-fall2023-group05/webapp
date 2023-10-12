aaaa66d151ff933391c86d9f0cd87f8f9c9baec3
Karan Wadhwa <wadhwa.karan96@gmail.com>
2023-10-12 19:54:27 -0400

    fix: http error codes
    fixes issue #8

--- src/api/routes/health.route.js
+++ src/api/routes/health.route.js
@@ -4,5 +4,8 @@ import { health } from '../controllers/health.controller'
 const router = express.Router()
 
 router.get('/healthz', health)
+router.all('/healthz', (req, res) => {
+  res.sendStatus(405).json()
+})
 
 export { router as healthRoute }
